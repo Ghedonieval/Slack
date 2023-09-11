0417f5375700494489811602e70e948ba6b66a7e
ghedonieval <asikongghedoni@gmail.com>
2023-09-11 13:48:45 +0100

    slack 1 commit

--- src/main/java/com/example/slack/SlackController.java
+++ src/main/java/com/example/slack/SlackController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
@@ -22,15 +23,21 @@ public class SlackController {
 
             // Get current day of the week
             String currentDay = OffsetDateTime.now(ZoneOffset.UTC).getDayOfWeek().toString();
+            String formattedDay = currentDay.substring(0, 1).toUpperCase() + currentDay.substring(1).toLowerCase();
+
 
             // Get current UTC time with a +/-2 minute window
-            DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
-            String currentUtcTime = OffsetDateTime.now(ZoneOffset.UTC).minusMinutes(2).format(formatter);
+//            DateTimeFormatter formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+//            String currentUtcTime = OffsetDateTime.now(ZoneOffset.UTC).minusMinutes(2).format(formatter);
+            Instant currentTime = Instant.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            String currentUtcTime = currentTime.atOffset(ZoneOffset.UTC).format(formatter);
+
 
             // GitHub URLs
             String githubFileUrl = "https://github.com/Ghedonieval/Slack/tree/main/src/main/java/com/example/slack";
             String githubRepoUrl = "https://github.com/Ghedonieval/Slack";
 
-            return new SlackModel(slack_name, currentDay, currentUtcTime, track, githubFileUrl, githubRepoUrl, 200);
+            return new SlackModel(slack_name, formattedDay, currentUtcTime, track, githubFileUrl, githubRepoUrl, 200);
         }
 }
